4a06ba1da4320a8eaefb01b05e0d9e70212ce51a
Jamie Bhakta <jamie@host-162-34.eduroam-local.wifi.le.ac.uk>
2020-01-28 11:21:53 +0000

    Leicester City Football Club Scouting App Skeleton

---
+++ lcfc-scoutingapp/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './LCFC-Print.png';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+      
+        <form>
+        <p></p>
+        
+        <label for="uname"><b>Username</b></label>
+        
+        <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+        <br></br>
+
+        <label for="pword"><b>Password</b></label>
+        
+        <input type="password" placeholder="Enter Password" name="pword" required ></input>
+
+        <br></br>
+
+        <button type="button" class="submit">Submit</button>
+
+        </form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
