469a92d0107c26544f3775e9e667eac298a3caf5
Jamie Bhakta <jamie@Jamies-MacBook-Pro.local>
2020-01-30 18:41:44 +0000

    Changed Div Tag Names

--- lcfc-scoutingapp/src/Components/LoginForm/LoginForm.js
+++ lcfc-scoutingapp/src/Components/LoginForm/LoginForm.js
@@ -2,30 +2,35 @@ import React from 'react';
 import './LoginForm.css'
 import logo from '../../Images/LCFC-Print.png';
 
-class Login extends React.Component {
-    render() {
-        return (  
-            <div class = "LoginForm">    
-                <img src={logo} className="App-logo" alt="logo" />
-                <form>
-                    <p></p>                    
-                    <label for="uname"><b>Username</b></label>
-                    <br/>
-                    <input type="text" placeholder="Enter Username" name="uname" required></input>
+function App() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        
+        <img src={logo} className="Login-logo" alt="logo" />
+      
+        <form>
+        <p></p>
+        
+        <label for="uname"><b>Username</b></label>
+        
+        <input type="text" placeholder="Enter Username" name="uname" required></input>
 
-                    <br/>
+        <br></br>
 
-                    <label for="pword"><b>Password</b></label>
-                    <br/>
-                    <input type="password" placeholder="Enter Password" name="pword" required ></input>
+        <label for="pword"><b>Password</b></label>
+        
+        <input type="password" placeholder="Enter Password" name="pword" required ></input>
 
-                    <br/>
+        <br></br>
 
-                    <button type="button" class="submit" onClick={{ }}>Submit</button>
-                </form>
-                
-        </div> 
-        );
-    }
+        <button type="button" class="submit">Submit</button>
+
+        </form>
+
+      </header>
+    </div>
+  );
 }
-export default Login;
+
+export default App;
