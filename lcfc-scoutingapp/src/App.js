ebd22f5351fdc38fa0dd5d48ba1a3097eedee1f9
mihirp123 <45435179+mihirp123@users.noreply.github.com>
2020-01-30 16:50:04 +0000

    FIXED ALL

--- lcfc-scoutingapp/src/App.js
+++ lcfc-scoutingapp/src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import logo from './Images/LCFC-Print.png';
 import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm'
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
-        
-        <img src={logo} className="App-logo" alt="logo" />
-      
-        <form>
-        <p></p>
-        
-        <label for="uname"><b>Username</b></label>
-        
-        <input type="text" placeholder="Enter Username" name="uname" required></input>
-
-        <br></br>
-
-        <label for="pword"><b>Password</b></label>
-        
-        <input type="password" placeholder="Enter Password" name="pword" required ></input>
-
-        <br></br>
-
-        <button type="button" class="submit">Submit</button>
-
-        </form>
-
-      </header>
+      <LoginForm />
     </div>
   );
 }
