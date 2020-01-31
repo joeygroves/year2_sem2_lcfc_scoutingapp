db2b45ddff30444abca6de2ae4615f51cfcfcd26
mihirp123 <45435179+mihirp123@users.noreply.github.com>
2020-01-31 22:19:57 +0000

    Folders for pages and reports

---
+++ lcfc-scoutingapp/src/Pages/PlayerReport.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class PlayReportPage extends React.Component{
+    render(){
+        return(
+            <div class= "Player Reports Forms">
+                <div class="dropdown">
+                    <input>Player Position:</input>
+                </div>
+            </div>
+        );
+    }
+}
+export default PlayReportPage
