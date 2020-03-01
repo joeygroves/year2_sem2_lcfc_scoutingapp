92db1d196cc241c2f15f02db092a22359f632c0e
Jamie Bhakta <jamie@Jamies-MacBook-Pro.local>
2020-03-01 21:41:37 +0000

    Wide Midfielder Working Update

--- App/LCFC-ScoutingApp/back-end/server.js
+++ App/LCFC-ScoutingApp/back-end/server.js
@@ -98,16 +98,16 @@ app.post('/api/GetScoutsReports', (req, res) => {
 
   if (req.session.loggedin) {
 
-    connection.query("select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from goalkeeper_reports inner join player where goalkeeper_reports.player_id = player.player_id and goalkeeper_reports.scouted_by = ?",
-      [req.session.username], function (error, results, fields) {
+    connection.query("select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join goalkeeper_reports on goalkeeper_reports.player_id = player.player_id and goalkeeper_reports.scouted_by = ? union select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join wide_midfielder_reports on wide_midfielder_reports.player_id = player.player_id and wide_midfielder_reports.scouted_by = ? union select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join centre_back_reports on centre_back_reports.player_id = player.player_id and centre_back_reports.scouted_by = ? union select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join centre_midfielder_reports on centre_midfielder_reports.player_id = player.player_id and centre_midfielder_reports.scouted_by = ? union select player.first_name AS 'FirstName',player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join full_back_reports on full_back_reports.player_id = player.player_id and full_back_reports.scouted_by = ? union select player.first_name AS 'FirstName' ,player.last_name AS 'LastName',player.club AS 'Club',player.age AS 'Age' from player inner join striker_reports on striker_reports.player_id = player.player_id and striker_reports.scouted_by = ?",
+      [req.session.username,req.session.username,req.session.username,req.session.username,req.session.username,req.session.username], function (error, results, fields) {
 
-        
+
 
         res.send(results);
 
 
         console.log(results);
-        
+
 
       });
 
@@ -178,7 +178,7 @@ app.post('/api/goalkeeper', (req, res) => {
   var reaction_to_mistake = req.body.reaction_to_mistake;
   var rating = req.body.rating;
   var notes = req.body.notes;
-  var playerID;
+
 
 
   var PlayerSQL = "INSERT INTO lcfc_scouting.player (first_name,last_name,club,height,age,position) VALUES ?";
@@ -188,12 +188,12 @@ app.post('/api/goalkeeper', (req, res) => {
     if (err) throw err;
   });
 
-  var PlayerIDSQL = "SELECT player_id FROM player where first_name = ? AND last_name = ?";
-  connection.query(PlayerIDSQL, [first_name, last_name], function (err, results) {
+  var PlayerIDSQL = "SELECT player_id FROM player where first_name = ? AND last_name = ? AND club = ? AND CAST(height AS DECIMAL) = CAST(? AS DECIMAL) AND age = ? AND position = ?";
+  connection.query(PlayerIDSQL, [first_name, last_name, club_name, height, age, position], function (err, results) {
     if (err) {
       throw err;
     } else {
-      
+
       console.log(results.length);
       console.log();
 
@@ -208,8 +208,94 @@ app.post('/api/goalkeeper', (req, res) => {
     };
 
 
-})
-  }
+  })
+}
+);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------ Insert Wide Fielder -------------------------------------
+
+app.post('/api/widemidfielder', (req, res) => {
+
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  var club_name = req.body.club_name;
+  var height = req.body.height;
+  var age = req.body.age
+  var date_played = req.body.date_played;
+  var club_played = req.body.club_played;
+  var ht_score = req.body.ht_score;
+  var ft_score = req.body.ft_score;
+  var position = 'Wide Midfielder'
+
+  var scouted_by = req.session.username;
+  var control = req.body.control;
+  var receiving_under_pressure = req.body.receiving_under_pressure;
+  var short_passing = req.body.short_passing;
+  var switching_play = req.body.switching_play;
+  var right_foot = req.body.right_foot;
+  var left_foot = req.body.left_foot;
+  var attacking_one_v_one = req.body.one_vs_one_attacking;
+  var attacking_ariel_ability = req.body.attacking_ariel_ability;
+  var dribbling = req.body.dribbling;
+  var shooting = req.body.shooting;
+  var crossing = req.body.crossing;
+  var defending_one_v_one = req.body.one_vs_one_defending;
+  var defending_ariel_ability = req.body.defending_ariel_ability;
+  var supporting_full_back = req.body.supporting_full_back;
+  var tackling = req.body.tackling;
+  var pressing = req.body.pressing;
+  var positional_awareness = req.body.positional_awareness;
+  var recovery_runs = req.body.recovery_runs;
+  var tracking_runners = req.body.tracking_runners;
+  var agility = req.body.agility;
+  var coming_in_off_the_line = req.body.coming_in_off_the_line;
+  var finding_space_out_wide = req.body.finding_space_out_wide;
+  var link_up_with_full_back = req.body.link_up_with_full_back;
+  var willingness_to_get_forward = req.body.willingness_to_get_forward;
+  var pace = req.body.pace;
+  var speed_when_dribbling = req.body.dribbling;
+  var strength = req.body.strength;
+  var work_rate = req.body.work_rate;
+  var bravery = req.body.bravery;
+  var leadership = req.body.leadership;
+  var teamwork = req.body.team_work;
+  var communication = req.body.communication;
+  var response_to_criticism = req.body.response_to_criticism;
+  var reaction_to_mistakes = req.body.reaction_to_mistakes;
+  var rating = req.body.rating;
+  var notes = req.body.notes;
+
+
+  var PlayerSQL = "INSERT INTO lcfc_scouting.player (first_name,last_name,club,height,age,position) VALUES ?";
+  var PlayerValues = [[first_name, last_name, club_name, height, age, position]]
+
+  connection.query(PlayerSQL, [PlayerValues], function (err, result) {
+    if (err) throw err;
+  });
+
+  var PlayerIDSQL = "SELECT player_id FROM player where first_name = ? AND last_name = ? AND club = ? AND CAST(height AS DECIMAL) = CAST(? AS DECIMAL) AND age = ? AND position = ?";
+  connection.query(PlayerIDSQL, [first_name, last_name, club_name, height, age, position], function (err, wmresults) {
+    if (err) {
+      throw err;
+    } else {
+
+      console.log(wmresults.length);
+      console.log();
+
+      var sql = "INSERT INTO lcfc_scouting.wide_midfielder_reports (player_id,scouted_by,control,recieving_under_pressure,short_passing,switching_play,right_foot,left_foot,attacking_one_v_one,attacking_ariel_ability,dribbling,shooting,crossing,defending_one_v_one,defending_ariel_ability,supporting_full_back,tackling,pressing,positional_awareness,recovery_runs,tracking_runners,agility,coming_in_off_the_line,finding_space_out_wide,link_up_with_full_back,willingness_to_get_forward,pace,speed_when_dribbling,strength,work_rate,bravery,leadership,teamwork,communication,response_to_criticism,reaction_to_mistakes,rating,notes)  VALUES ?";
+      var values = [[wmresults[0].player_id, scouted_by, control, receiving_under_pressure, short_passing, switching_play, right_foot, left_foot, attacking_one_v_one, attacking_ariel_ability, dribbling, shooting, crossing, defending_one_v_one, defending_ariel_ability, supporting_full_back, tackling, pressing, positional_awareness, recovery_runs, tracking_runners, agility, coming_in_off_the_line, finding_space_out_wide, link_up_with_full_back, willingness_to_get_forward, pace,
+        speed_when_dribbling, strength, work_rate, bravery, leadership, teamwork, communication, response_to_criticism, reaction_to_mistakes, rating, notes]];
+
+      connection.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+      });
+
+    };
+
+
+  })
+}
 );
 
 // --------------------------------------------------------------------------------------------------------------------- Server --------------------------------------------------------------------------
