84a28d28e1cad7b572a5eb8c8373fd6ee3e6f02a
unknown <tsr10@student.le.ac.uk>
2020-03-07 19:41:15 +0000

    Updated get_ScoutsReports.js in back-end to email csv

--- App/LCFC-ScoutingApp/back-end/route_handlers/get/get_ScoutsReports.js
+++ App/LCFC-ScoutingApp/back-end/route_handlers/get/get_ScoutsReports.js
@@ -57,8 +57,7 @@ module.exports = function(app) {
         attachments: [
           {
             filename: "test.csv",
-            content: "Name",
-            content: "Club"
+            content: results
           }
         ]
       };
