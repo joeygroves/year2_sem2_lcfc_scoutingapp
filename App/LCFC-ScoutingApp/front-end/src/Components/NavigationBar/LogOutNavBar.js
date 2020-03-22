2a803242e69c239473093646c5e74324bd2ca762
Jamie Bhakta <jamie@jamies-mbp.home>
2020-03-22 17:08:09 +0000

    Updated NavBar for logOut

--- App/LCFC-ScoutingApp/front-end/src/Components/NavigationBar/LogOutNavBar.js
+++ App/LCFC-ScoutingApp/front-end/src/Components/NavigationBar/LogOutNavBar.js
@@ -12,57 +12,59 @@ import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 class navBar extends React.Component {
 
     state = {
-      
+
         username: '',
-      
+
         body: '',
-    
+
     };
 
 
     myFunction = () => {
-      
+
         var x = document.getElementById("myTopnav");
-     
+
         if (x.className === "topnav") {
-        
+
             x.className += " responsive";
-       
+
         } else {
-       
+
             x.className = "topnav";
-       
+
         }
     }
 
- 
+
     render() {
 
         return (
             <div class="NavigationBar">
-              
+
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-               
+
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-               
+
                 <div className="topnav" id="myTopnav">
-                   
+
                     <div class="topLogo"><img src={imageNav} alt="navImage"></img></div>
-                
+
                     { /* eslint-disable-next-line */}
                     <a href="javascript:void(0);" class="icon" onClick={this.myFunction}>
-                    
-                        <i class="fa fa-bars"></i>
-                   
-                    </a>
-                  
-                   
-                    {/* eslint-disable-next-line */}
-                    <a class="right"><Link to={"/LogBackIn"}>Log Back In</Link></a>
 
-              
+                        <i class="fa fa-bars"></i>
+
+                    </a>
+
+                    <div class="links">
+
+                        {/* eslint-disable-next-line */}
+                        <a class="right"><Link to={"/LogBackIn"}>Log Back In</Link></a>
+
+                    </div>
+
                 </div>
-          
+
             </div>
 
         );
